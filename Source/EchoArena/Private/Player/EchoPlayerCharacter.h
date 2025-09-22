3794f7d89cc901baa2d08686a6347ecdda9e7e32
VanshJain609 <jainvansh178@gmail.com>
2025-09-22 19:03:45 +0530

    base

---
+++ Source/EchoArena/Private/Player/EchoPlayerCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EchoArena/Character/EchoCharacter.h"
+#include "EchoPlayerCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AEchoPlayerCharacter : public AEchoCharacter
+{
+	GENERATED_BODY()
+	
+};
